371fda544d7d9132b9c37f45f999f2014f080314
Gage <gagemigan@gmail.com>
2020-06-29 15:32:46 -0400

    "working as intented"

--- src/cal.js
+++ src/cal.js
@@ -1,30 +1,52 @@
-const sum = (arr) => {
-  let res = 0 ;
-  if(arr.length){
-    arr.map(add => res += add);
-  }
-  return res;
-}; 
-
-const mealDetails = (price,tax ,tip) => {
-  let total = 0;
-  const tipPercent = tip * 0.01 ;
-  const taxPercent = tax * 0.01 ;
-  if( tax <= 0  || undefined){
-    return 0;
-  } else {
-    total += taxPercent * price ; 
-  }
-  if( tip <= 0  || undefined){
-    return 0;
-  } else {
-    total += tipPercent * price ; 
-  }
-  return total += price ; 
-
+const store = {
+  tip : 0,
+  tax : 0,
+  meals : [],
+  tipJar : [],
 };
 
+const tipSum = () =>{
+  if(store.tipJar.length){
+    return store.tipJar.reduce( (a,b) => a+b );
+  }else{
+    return 0;
+  }
+};
+
+const mealsSum = () =>{
+  if(store.meals.length){
+    return store.meals.reduce((x,y) => x + y);
+  }else{
+    return 0;
+  }
+} 
+
+const changeTipVal = (tip) => store.tip = tip; 
+
+const changeTaxVal = (tax) => store.tax = tax;
+
+const handleClear = () =>{
+  store.meals.splice(0,store.meals.length);
+  store.tipJar.splice(0,store.tipJar.length);
+  store.tax = 0;
+  store.tip = 0;
+}; 
+
+const handleSubmit = (newItem , tip) => {
+  store.tipJar.push(tip);
+  store.meals.push(newItem);
+} ;
+
+const subTotal = () => mealsSum() + (store.tax  * mealsSum() );
+
+
 export default{
-  sum,
-  mealDetails: mealDetails,
+  store,
+  mealsSum,
+  tipSum,
+  subTotal,
+  handleClear,
+  handleSubmit,
+  changeTipVal,
+  changeTaxVal
 };
--- src/events.js
+++ src/events.js
@@ -1,9 +1,106 @@
-import cal from './cal';
+import cal from './cal.js';
 
-let store = [];
+const mealDetailTemplate = () =>{
+  return (
+    `<div class="meal-enter">
+            <h1>Enter the Meal Details</h1>
+            <form id='meal-form'>
+                <label for='price'> Base Meal Price : $</label>
+                    <input type="number" id="price" required />
+                <label for='tax'> Tax rate : %</label>
+                    <input type="number" id="tax" value="6"/>
+                <label for='tip'> Tip Rate: %</label>
+                    <input type="number" id="tip" value="15"/>
+                <button type="submit" id='submit'>Submit</button>
+                <button type="reset" id='cancel'>Cancel</button>
+            </form>
 
-const handleClear = () => store = [];
+        </div>`
+  );
+};
 
-const handleSubmit = () => {
-    
-} ;
+const customerChargeTemplate = () =>{
+  return(
+    `<div class="subtotal">
+            <div >
+            <h2> Customer Charges</h2>
+                <span>Subtotal</span> 
+                <span>$${cal.subTotal()}</span>
+            </div>
+            <div class="tip-total">
+                <span>Tip</span> 
+                <span>$${cal.tipSum()}</span> 
+            </div>
+            <div class="total">
+                <span>Total</span> 
+                <span>$${cal.tipSum() + cal.subTotal()}</span>
+            </div>
+        </div>`
+
+  );
+};
+
+const earningsTemplate = () =>{
+  const avgTip = () => cal.store.meals.length === 0  ? '': cal.tipSum() / cal.store.meals.length;
+  return (
+    `<div class='earnings'>
+        <h2>My Earning info</h2>
+            <div>
+                <span>Tip Total:</span>
+                <span>$${cal.tipSum()}</span>
+            </div>
+            <div>
+                <span>Meal count:</span>
+                <span>${cal.store.meals.length}</span>
+            </div>
+            <div>
+                <span>Average Tip Per Meal</span>
+                <span>$${avgTip()}</span>
+                <button type="reset" class='clear'>reset</button>
+            </div>
+    </div>` 
+  );
+};
+
+const render = () => {
+  let html = '';
+  html += mealDetailTemplate();
+  html += customerChargeTemplate();
+  html += earningsTemplate();
+  
+  $('.root').html(html);
+};
+
+const handleSubmitClick = () => {
+  $('.root').on('click', '#submit', (e) => {
+    e.preventDefault();
+    const price = parseInt($('#price').val());
+    const tip =  0.01 * parseInt($('#tip').val());
+    const tax =  0.01 * parseInt($('#tax').val());
+    const tipTotal = price * tip; 
+    cal.changeTipVal(tip);
+    cal.changeTaxVal(tax);
+    cal.handleSubmit(price, tipTotal);
+    $('#price').val('');
+    render();
+  });
+};
+
+const handleResetClick = () => {
+  $('.root').on('click','button.clear', (e)=> {  
+    e.preventDefault();
+    $('#price').val('');
+    cal.handleClear();
+    render();
+  });
+};
+
+const eventPackage = () => {
+  render();
+  handleSubmitClick();
+  handleResetClick();
+};
+
+export default{
+  eventPackage,
+};
